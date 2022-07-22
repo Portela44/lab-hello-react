48128133b7da3c50bdc3d97072c1b47cf8a741a6
Guillem <guillem.mart7@gmail.com>
2022-07-22 12:55:05 +0200

    landing part done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import Landing from './components/Landing';
 
 function App() {
   return (
-    <div className="App"></div>
+    <div className="App">
+      <Landing/>
+    </div>
   );
 }
 
---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Header() {
+  return (
+    <div className="header">
+        <h1>Say hello to ReactJS</h1>
+        <h5>You will learn how to use the most popular frontend library, and become a super Ninja developer.</h5>
+        <button>Awesome!</button>
+    </div>
+  )
+}
